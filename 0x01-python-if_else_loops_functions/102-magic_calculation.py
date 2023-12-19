f7cf1377dc6f9e58cf4b407be8541073c3fc5c21
Paul Chukwujekwu <paulchukwu2001@gmail.com>
2023-12-18 20:07:13 -0800

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
-def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return c
+def magic_calculation(a, b, c):
+    if (a < b):
+        return (c)
+    elif (c > b):
+        return (a + b)
     else:
-        return sub(a, b)
+        return (a * b - c)
